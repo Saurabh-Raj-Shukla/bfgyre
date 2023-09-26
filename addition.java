cf4bee5935ec8721fa22a7d0711cefa70afb5f07
Saurabh Raj Shukla <shuklasaurabhraj@gmail.com>
2023-09-26 12:58:01 +0530

    First commit

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    /**
+     * @param args
+     */
+    public static void main (String args [])
+    {
+        System.out.print("Enter any two numbers to add: ");  
+    }
+}
